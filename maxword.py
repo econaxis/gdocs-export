2e73423d2701d4caafd30e34e0d5c3eb1483e110
Henry <henry2833@gmail.com>
2020-03-31 14:59:52 -0700

    Small table, colored progress, exports to HTML for maxword.py
    Former-commit-id: 1f1971b3a0e7c80a0aa5b66bc0f41af07c18811e
    Former-commit-id: 6d5f7b11c55bae4e5da7c3183e15c8a4a1b716a0
    Former-commit-id: 81f4a57701aad8e32eac67bd2d6adceacbc1660e [formerly 54d052d27fba5507c3832ab6d544944748693d8c]
    Former-commit-id: 72a8582dabfd631ef6800d3f67f56e34abc7a7da

--- maxword.py
+++ maxword.py
@@ -5,13 +5,17 @@ from load import append_df_to_excel
 import math
 from collections import OrderedDict
 from pprint import PrettyPrinter
+import seaborn as sns
 from datetime import datetime, timezone
+import bisect
 import numpy
 import matplotlib.pyplot as plt
 import requests
 import os.path
 import pandas as pd
 from pathlib import Path
+
+DBG_LIMIT = 200
 
 pp = PrettyPrinter(indent=3)
 EXCEL_PATH = 'text.xlsx'
@@ -48,7 +52,7 @@ def main():
 
 
 
-	for (index, f) in enumerate(cols[0:]):
+	for (index, f) in enumerate(cols[:DBG_LIMIT]):
 		#Iterate through files
 		prevMap = [dates_map[dates[0]], -1, -1]
 		processed.append(f)
@@ -86,37 +90,113 @@ def main():
 				
 
 			#	Function for filling in cells between
-				for betweenCells in timeBuckets[prevD:curD]:
-					maxWords.loc[betweenCells,f] = prevMap[1]					
+			#	for betweenCells in timeBuckets[prevD:curD]:
+			#		maxWords.loc[betweenCells,f] = prevMap[1]					
 
 
 				#Reset prevMap
 				prevMap[0] = dates_map[d]
 				prevMap[2] = prevMap[1]
 				prevMap[1] = -1
-				if(d == dates[-1]):
+				#if(d == dates[-1]):
 					#Last date reached, special case
-					maxWords.loc[timeBuckets[prevMap[0]], f] =csvdata.loc[trimmedDates[dat_ind-1], f]
+					#maxWords.loc[timeBuckets[prevMap[0]], f] =csvdata.loc[trimmedDates[dat_ind-1], f]
+	idx = pd.IndexSlice
 
-
-	pickle.dump(maxWords, open('maxwords.pickle', 'wb'))
 	pp.pprint(maxWords)
+	maxWords = maxWords.iloc[:, 0:DBG_LIMIT]
 
 
-	with open('maxwords.pickle', 'wb') as mw:
-		pickle.dump(maxWords, mw)
-	append_df_to_excel('text.xlsx',maxWords, sheet_name = "maxWords", truncate_sheet=True)
+	subset = maxWords.index.difference(["Highest Word Count"])
+	mwStyler = maxWords.style \
+	.apply(mwHighlighter, axis=0, subset = idx[subset, :])
+	print("bet")
+	mwStyler = mwStyler \
+	.apply(mwHighlighter,axis = 1,subset=idx["Highest Word Count", :])\
+ 	.set_precision(2)\
+	.set_table_styles([{
+		'selector': 'table, th, td',
+		'props': [
+			("table-layout", "fixed"),
+			("border-collapse", "collapse"),
+			("overflow", "hidden"),
+			("max-width", "3px"),
+			("max-height", "4px"),
+			("font-size", "3px")
+		]
+	}])
 
-	pd.set_option('display.max_colwidth', 10)
-	maxWords.to_html('i.html')
-	csvdata.to_html('csv.html')
+
+
+
+
+	open('style.html', 'w').write(mwStyler.render())
+
+#	pickle.dump(maxWords, open('maxwords.pickle', 'wb'))
+#	pp.pprint(maxWords)
+
+
+#	with open('maxwords.pickle', 'wb') as mw:
+#		pickle.dump(maxWords, mw)
+	
+
+	#append_df_to_excel('text.xlsx',maxWords, sheet_name = "maxWords", truncate_sheet=True)
+
+#	pd.set_option('display.max_colwidth', 10)
+#	maxWords.to_html('i.html')
+
 
 def mwHighlighter(x):
 	#Applies styling to dataframe maxWords
+	numColors = 300
 
-	df = x.copy()
+
+	#Create colormap to only retain medial values
+	MED_VALUE = 50
+	cm = sns.color_palette("Blues", numColors+2*MED_VALUE+1).as_hex()
+	del cm[:MED_VALUE]
+	del cm[-MED_VALUE:]
+
+	
+	st = x.copy()
+	st[:] = 0
+	words = sorted(list(x.dropna()))
+	i0 = bisect.bisect_right(words, 0)
+	del words[:i0]
+
+	if(len(words)==0):
+		#Empty list after preprocessing
+		st[:] = 'color: black; opacity: 0.1'
+		return st
+
+	minW = max(words[0],1)
+
+	#For reasonble colors
+	maxW = int(max(words[-1], 1)) 
+	#print("%d %d"%(minW, maxW))
+	diff = maxW - minW
+	#pp.pprint("%d %d %d"%(minW, diff, maxW))
+
+	sameColorMode = False
+	if(diff == 0):
+		sameColorMode = True
 
 
+	for (index,val) in enumerate(x):
+		
+		if(math.isnan(val) or val==0):
+			st[index] = 'color: black; opacity: 0.1'
+			continue
+
+		if(sameColorMode):
+			st[index] = 'background-color: '+ cm[numColors-1]
+			continue
+		cmapIndex = min(int((val-minW)/diff * numColors), numColors-1)
+		
+		st[index] = 'background-color: ' + cm[cmapIndex]
+	
+	#pp.pprint(st)
+	return st
 
 if __name__ == "__main__":
 	main()
