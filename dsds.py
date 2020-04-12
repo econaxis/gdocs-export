7462f17254c6f1f881bde3bbab93fc7490f648c9
henry <henry2833@gmail.com>
2020-04-12 13:56:21 -0700

    envvar for debug dsds
    Former-commit-id: 8a0eac392eb7ad542788987008db5187a59f20cd [formerly 192cb1ddb3ca2ae312457ee71c9dc8b1993efb16]
    Former-commit-id: f7e9392fa7963f4899e658c6abcf9b84fc2e8e05
    Former-commit-id: 021521287543f118adc898d94f2be74388c1556c
    Former-commit-id: 9de2445e2b3ec29fda8a3565acf10c7ab0a33c19 [formerly 98965dcc9d1b5c2d256112e8e7338e901079aae1]
    Former-commit-id: 32e453586c542ee9b50026f8f9396a133f0f07bd

--- dsds.py
+++ dsds.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     uid = "527e4afc-4598-400f-8536-afa5324f0ba4"
     fileid = 'root'
     homePath =  "/home/henry/pydocs/"
+    if("DBGHPATH" in os.environ):
+        homePath = os.environ["DBGHPATH"]
     workingPath =  homePath + 'data/' + uid + '/'
     creds = pickle.load(open(workingPath + 'creds.pickle', 'rb'))
     loadFiles(uid, workingPath, fileid, creds)
