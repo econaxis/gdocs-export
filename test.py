0adc4bb2f74904ea9599a30a4402436ef35124c6
Henry <h>
2020-06-12 17:23:27 -0700

    ds
    Former-commit-id: a562ea96b3fd6dc955b88bb83b2e1bbaa34fa687 [formerly 2aaaf44ae73282d780575753e504155509191c63]
    Former-commit-id: 7142a78e74586f14cd0167bb2b38dc1309b13da6
    Former-commit-id: 345cceae0ed578812e01c700e14f6025c5cb99ce
    Former-commit-id: 01123c1e447f28a2c4d862f9895dd36ee177f539 [formerly 38f76ba70cfdd5aa9c4dc1d25a28dbcc5e0e5d12]
    Former-commit-id: caa6e1ae088422f8da9f1b929c9d5f5c0f23f8f3

--- test.py
+++ test.py
@@ -6,11 +6,12 @@ import subprocess
 
 
 def main():
-    for threads in range(2, 6):
-        for workers in range(3, 8):
+    for threads in range(2, 4):
+        for workers in range(3, 6):
             gdoc.threads = threads
             get_files.workerInstances = workers
             with tracer.span("LARGE TEST threads: {} ;; workers {}".format(threads, workers)):
-                print("outside threads: {} ;; workers {}".format(threads, workers))
-                subprocess.run(['python3.8', 'dsds.py', str(threads), str(workers)])
+                subprocess.run(['python3.8', 'dsds.py', '-t {}'.format(str(threads)), '-w {}'.format( str(workers)), '-m'])
+
+
 main()
