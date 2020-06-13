54c38a2328acba2d03bcf4b2e102116e01d62634
Henry <h>
2020-06-12 17:50:10 -0700

    ds
    Former-commit-id: 429b8f209ede4b5b2b9ea2f6eb59081f89a67cae [formerly 7ce12b0e20441f8207fec425e50839800c729ed1]
    Former-commit-id: df58278a68aec1080795d281f2a967a6bf36be20
    Former-commit-id: 84076e841b3cfeca1e7439ef1572ead125bfe66b
    Former-commit-id: 48229f1abb29cba8d18c6776b6d86395305fb238 [formerly 9c7306c131af4d491b79191fb4a8799e4872c822]
    Former-commit-id: 27aed0634ab676064e84dfe14910fcd7f0d6d601

--- test.py
+++ test.py
@@ -4,14 +4,35 @@ from processing import gdoc
 from configlog import tracer
 import subprocess
 
+def run(threads, workers, mart):
+    with tracer.span("LARGE TEST threads: {} ;; workers {}".format(threads, workers)):
+        if mart:
+            subprocess.run(['python3.8', 'dsds.py', '-t {}'.format(str(threads)), '-w {}'.format( str(workers)), '-m'])
+        else:
+            subprocess.run(['python3.8', 'dsds.py', '-t {}'.format(str(threads)), '-w {}'.format( str(workers))])
 
 def main():
-    for threads in range(2, 4):
+    for threads in range(2, 3):
         for workers in range(3, 6):
-            gdoc.threads = threads
-            get_files.workerInstances = workers
-            with tracer.span("LARGE TEST threads: {} ;; workers {}".format(threads, workers)):
-                subprocess.run(['python3.8', 'dsds.py', '-t {}'.format(str(threads)), '-w {}'.format( str(workers)), '-m'])
+            if threads > workers:
+                continue
+            import threading
+            
+            mart = lambda: run(threads, workers, False)
+            hen = lambda: run(threads, workers, True)
+
+            hent = threading.Thread(target = mart)
+            martt = threading.Thread(target= hen)
+
+            hent.start()
+            martt.start()
+
+            hent.join()
+            martt.join()
+
+
+            import time
+            time.sleep(30)
 
 
 main()
